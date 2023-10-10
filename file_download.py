b9bf47ab4e7a209e9bab38762010f3a1994f11ca
Gabriel Orozco <juan.orozco@sourcemeridian.com>
2023-10-10 09:22:09 -0500

    Now the file download waits for the download is finished without time sleep

--- file_download.py
+++ file_download.py
@@ -8,6 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
 
 from selenium import webdriver
+
+
+def download_finish(driver: Chrome):
+    if not driver.current_url.startswith("chrome://downloads"):
+        driver.get("chrome://downloads")
+    return driver.execute_script("""
+        var items = document.querySelector('downloads-manager')
+            .shadowRoot.getElementById('downloadsList').items;
+        if (items.every(e => e.state === "COMPLETE"))
+            return items.map(e => e.fileUrl || e.file_url);
+    """)
+
+
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/download")
 try:
@@ -20,6 +33,9 @@ else:
     element = driver.find_element(By.XPATH, "//div[@class='example']/a")
     text = element.text
     element.click()
-    time.sleep(5)
+try:
+    WebDriverWait(driver, 10).until(download_finish)
+except:
+    print("file was not downloaded")
+else:
     Path(f"/Users/juan.orozco/Downloads/{text}").rename(f"/Users/juan.orozco/Desktop/Automation practice/Automation_practice/{text}")
-    time.sleep(2)
